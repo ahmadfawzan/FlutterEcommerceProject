a6207c666e19bfac6dcf450d70470a94e3af726e
ahmadfawzan <ahmadhassan0566@gmail.com>
2022-12-19 22:39:33 +0300

    Flutter App

--- lib/DetailProdect.dart
+++ lib/DetailProdect.dart
@@ -4,7 +4,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 class DetailProdect extends StatefulWidget {
   const DetailProdect({Key? key, required DetailProdect}) : super(key: key);
 
-  @override
+  @override//A
   State<DetailProdect> createState() => _DetailProdectState();
 }
 class _DetailProdectState extends State<DetailProdect> {
